2db2e7a8233243c3d51edef57d4fc0a74fc45646
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-10-16 01:29:44 +0530

    dayy4

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //const http = require("http");
+const path = require("path");
 const express = require("express"); //import express
 
 const bodyParser = require("body-parser");
@@ -14,11 +15,11 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page Not Found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(5000);

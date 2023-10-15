45e1be8291d7711c8091996997e9285a93d57b14
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-10-15 11:16:02 +0530

    continue

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const express = require("express"); //import express
 
 const app = express(); //running express as a function
 
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); //Allows the request to continue to the next middleware in line
+}); //use => to add a new middleware function...it accepts array of request handlers
+
+app.use((req, res, next) => {
+  console.log("In another middleware");
+
+  res.send("<h1>Hello from Express!</h1>");
+  // send allows us to send a response
+});
 const server = http.createServer(app);
 
 server.listen(5000);

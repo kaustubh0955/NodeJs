83283e00c89dfa2fad8a6a46dfdad571d86e3ec0
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-10-20 01:16:21 +0530

    dayy6

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,8 @@ const adminData = require("./admin"); //Adding a import by importing admin data 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.render("shop");
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop" });
   // send allows us to send a response
 });
 

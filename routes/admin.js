5fc90324f80dc0493aeb024462516bde0b987ec8
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-10-20 01:35:10 +0530

    continue

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,22 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
 
-const rootDir = require("../util/path");
+const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 const products = [];
 
-// /admin/add-product==> GET
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", { pageTitle: "Add Product" });
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
-//admin /admin/add-product =>POST
-router.post("/add-product", (req, res, next) => {
-  //console.log(req.body);
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
   products.push({ title: req.body.title });
-  res.redirect("/");
+  res.redirect('/');
 });
 
 exports.routes = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,15 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
 
-const rootDir = require("../util/path");
+const express = require('express');
 
-const adminData = require("./admin"); //Adding a import by importing admin data and exporting its routes
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render("shop", { prods: products, docTitle: "Shop" });
-  // send allows us to send a response
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;

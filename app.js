3734d845b2d06facc89ab04ea4c387e677b81a44
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-10-20 20:58:27 +0530

    day7

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-//const http = require("http");
-const path = require("path");
-const express = require("express"); //import express
+const path = require('path');
 
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
-const app = express(); //running express as a function
+const app = express();
 
-app.set("view engine", "pug");
-app.set("views", "views");
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
-const adminData = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-//app.use((req, res, next) => {
-//console.log("In the middleware");
-//next(); //Allows the request to continue to the next middleware in line
-//}); //use => to add a new middleware function...it accepts array of request handlers
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminData.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-app.listen(5000);
+app.listen(3000);
